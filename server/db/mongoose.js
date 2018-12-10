6bc3e995a90e6c993734d71d05af8b8fde9f81be
Neville Li <nevilleneville@hotmail.com>
2018-12-10 15:10:05 -0600

    set up for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true }); 
+// mongoose.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true }); 
+mongoose.connect("mongodb+srv://m001-student:12345@sandbox-dkqfw.mongodb.net/TodoApp?retryWrites=true", { useNewUrlParser: true }); 
+
 
 
 module.exports = {
