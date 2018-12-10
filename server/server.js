6e70898b6f4fa3e8b4f53b5192c71119f746552e
Neville Li <nevilleneville@hotmail.com>
2018-12-09 23:28:12 -0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const {ObjectID} = require("mongodb");
 
 var {mongoose} = require("./db/mongoose.js");
 var {Todo} = require("./models/todo");
@@ -28,7 +29,28 @@ app.get("/todos", (req, res) => {
     });
 });
 
+//GET /todos/1234
 
+app.get("/todos/:id",(req, res) => {
+    var id = req.params.id;
+    
+    if(!ObjectID.isValid(id)) {
+        console.log("ID not valid");
+        return res.status(404).send({});
+    }
+
+    Todo.findById(id).then(todo => {
+        if(todo) {
+           return res.status(200).send({todo});
+        }
+        res.status(400).send({});
+    }).catch(e => res.status(400).send());
+    //validate id using isValid
+    // if not valid return 404, send back empty body
+
+    //findById //success case if todo send it back if no todo - send back 404
+    // error -> 400 send empty body
+});
 
 app.listen(3000, () => {
     console.log("Started on port 3000");
