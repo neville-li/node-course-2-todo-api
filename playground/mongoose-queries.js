f87e388cae792cfc95b1425f3427825d641bc25e
Neville Li <nevilleneville@hotmail.com>
2018-12-09 22:44:37 -0600

    add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,36 @@
+const {ObjectID} = require("mongodb");
+
+const {mongoose} = require("./../server/db/mongoose");
+const {Todo} = require("./../server/models/todo");
+const {User} = require("./../server/models/user");
+
+// var id = "5c0db714b730763539ea26c2ff";
+
+// if(!ObjectID.isValid(id)){
+//     console.log("ID not valid");
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log("Todos", todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log("Todo", todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if(!todo) {
+//         return console.log("Id not found");
+//     }
+//     console.log("Todo", todo);
+// }).catch((e) => console.log(e));
+
+var id = "5c0d78b5958ff6193b197059";
+
+User.findById(id).then((res) => {
+    console.log("Data fetched: ", res);
+});
