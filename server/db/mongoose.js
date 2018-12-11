b417a283e8374ab58c74fff8fca70e34d0650fa5
Neville Li <nevilleneville@hotmail.com>
2018-12-11 02:55:27 -0600

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true }); 
-//mongoose.connect("mongodb+srv://m001-student:12345@sandbox-dkqfw.mongodb.net/TodoApp?retryWrites=true", { useNewUrlParser: true }); 
-
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }); 
+//"mongodb://localhost:27017/TodoApp"
+//"mongodb+srv://m001-student:12345@sandbox-dkqfw.mongodb.net/TodoApp?retryWrites=true"
 
 
 module.exports = {
