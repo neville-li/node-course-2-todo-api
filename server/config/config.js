8eef0961a6811c27472116b8ded6fc2dc43a8640
Neville Li <nevilleneville@hotmail.com>
2018-12-11 16:39:19 -0600

    config mongodb uri

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,9 @@
 var env = process.env.NODE_ENV || "development";
 console.log("env **********" , env);
 
+if(env === "production") {
+    process.env.MONGODB_URI = "mongodb+srv://m001-student:12345@sandbox-dkqfw.mongodb.net/TodoApp?retryWrites=true";
+}
 if(env === "development") {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
