d0e107f816a12ea75e6e4d7d1af8a1f7b1444623
Neville Li <nevilleneville@hotmail.com>
2018-12-12 18:43:40 -0600

    Add GET/ users/me and authentication middleware

--- playground/hashing.js
+++ playground/hashing.js
@@ -6,8 +6,8 @@ var data = {
 };
 
 var token = jwt.sign(data, "123abc");
-var decoded = jwt.verify(token, "123abc");
-console.log(`decoded`, decoded);
+var decoded = jwt.verify(token, "123abc"); // return payload or throw error if salt is not correct
+console.log(`decoded`, decoded); // get payload
 
 console.log(token);
 
